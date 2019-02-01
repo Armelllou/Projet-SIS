2f359029e19d3743b139a7fcaaceaff945aae5d5
Kawtar <kstifine@gmail.com>
2019-02-01 10:04:33 +0100

    BD

--- src/NF/Main.java
+++ src/NF/Main.java
@@ -1,9 +1,20 @@
 package fr.polytech.tis.sis;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //a
+        try{
+            // 1. Get a connection to database
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dem", "id", "mdp");
+            //2. Create a statement
+            //3. Execute SQL query
+            //4. Process the result set
+        }
+        catch (Exception exc){
+            exc.printStackTrace();
+        }
     }
 }
