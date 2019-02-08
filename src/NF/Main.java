0cda5870516bb748825829d44c48a43023005462
Armelle Bernard <armellebernard@free.fr>
2019-02-08 11:49:25 +0100

    Connexion java <--> BD done
    AJout classe ConnexionBD pour se connecter à labd bd1

--- src/NF/Main.java
+++ src/NF/Main.java
@@ -1,5 +1,8 @@
 package NF;
 
+import BD.ConnexionBD;
+import interfaces.Connexion;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,43 +10,14 @@ import java.sql.Statement;
 
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args){
+        Ipp ipp = new Ipp();
+        Ipp ipp3 = new Ipp();
+        Ipp ipp4 = new Ipp();
+        Ipp ipp5 = new Ipp();
+        Ipp ipp6 = new Ipp();
 
-        String url = "jdbc:mysql://localhost:3306/bd1";
-        String login = "root";
-        String passwd = "";
-        Connection cn = null;
-        Statement st = null;
+        ConnexionBD conn = new ConnexionBD();
 
-        //try {
-            // 1. Get a connection to database
-            //Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dem", "id", "mdp");
-            //Class.forName("com.mysql.jdbc.Driver");
-            //cn = DriverManager.getConnection(url, login, passwd);
-            //st = cn.createStatement();
-            // Chargement du driver
-            // Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
-
-            // Connexion à la base de données "DBurl"
-            // this.conn = DriverManager.getConnection(String.valueOf(myConn));
-
-            //if (this.conn.isValid(0)) {
-            System.out.println("===>Connexion effectuée");
-
-            //3. Execute SQL query
-            //4. Process the result set
-            //  }
-
-            // } catch (IllegalAccessException | InstantiationException | SQLException e) {
-            //    e.printStackTrace();
-            // }
-
-            Ipp ipp = new Ipp();
-            Ipp ipp3 = new Ipp();
-            Ipp ipp4 = new Ipp();
-            Ipp ipp5 = new Ipp();
-            Ipp ipp6 = new Ipp();
-        } //catch (SQLException e) {
-           // e.printStackTrace();
-        }
-    //}}
+    }
+}
