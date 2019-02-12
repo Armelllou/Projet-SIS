8a1c145eb2dc23dc44ee5c4afe3bf19719a604df
Manon Caulier <caulier.manon18@orange.fr>
2019-02-12 22:05:53 +0100

    Interfaces réussies

--- src/interfaces/AccueilSecretaire.java
+++ src/interfaces/AccueilSecretaire.java
@@ -5,14 +5,11 @@
  */
 package interfaces;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 /**
  *
  * @author Manon
  */
-public class AccueilSecretaire extends javax.swing.JPanel  {
+public class AccueilSecretaire extends javax.swing.JPanel {
 
     /**
      * @return the jButton1
@@ -21,19 +18,19 @@ public class AccueilSecretaire extends javax.swing.JPanel  {
         return jButton1;
     }
 
-  
-    
+    /**
+     * @return the jButton2
+     */
     public javax.swing.JButton getjButton2() {
         return jButton2;
     }
 
-
-    
-    
     /**
-     * Creates new form AccueilSecretaire
+     * Creates new form AccueilSecretaire2
      */
-  
+    public AccueilSecretaire() {
+        initComponents();
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -44,14 +41,12 @@ public class AccueilSecretaire extends javax.swing.JPanel  {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 102, 204), 10, true));
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/interfaces/logo.png"))); // NOI18N
 
         jButton1.setFont(new java.awt.Font("Calibri", 1, 36)); // NOI18N
         jButton1.setText("Créer un nouveau dossier patient");
@@ -59,32 +54,38 @@ public class AccueilSecretaire extends javax.swing.JPanel  {
         jButton2.setFont(new java.awt.Font("Calibri", 1, 36)); // NOI18N
         jButton2.setText("Consulter un dossier patient existant");
 
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/interfaces/logo.png"))); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(531, 531, 531)
-                        .addComponent(jButton1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(501, 501, 501)
-                        .addComponent(jButton2)))
-                .addContainerGap(602, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(531, 531, 531)
+                                .addComponent(jButton1))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(505, 505, 505)
+                                .addComponent(jButton2)))
+                        .addGap(0, 588, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(144, 144, 144)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(188, 188, 188)
                 .addComponent(jButton1)
-                .addGap(75, 75, 75)
+                .addGap(62, 62, 62)
                 .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 258, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 270, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -94,6 +95,4 @@ public class AccueilSecretaire extends javax.swing.JPanel  {
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
-
-     
 }
