9c1db4c070da978e2f180f3f9d2b5afe9b66df3a
Armelle Bernard <armellebernard@free.fr>
2019-02-17 18:06:11 +0100

    Recherche par Nom + prénom

--- src/Listener/BoutonRecherchePatient.java
+++ src/Listener/BoutonRecherchePatient.java
@@ -25,13 +25,13 @@ public class BoutonRecherchePatient implements ActionListener {
 
 
     @Override
-    public void actionPerformed(ActionEvent e)  {
+    public void actionPerformed(ActionEvent e) {
 
         try {
             String ipp = cdpis.getjTextField1().getText();
             String Sql1 = "Select * FROM patient WHERE IPP ='" + ipp + "'";
 
-            java.sql.Connection conn1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd1"+"?serverTimezone=UTC","armelle","armelle");
+            java.sql.Connection conn1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd1" + "?serverTimezone=UTC", "armelle", "armelle");
             Statement stmt = null;
             stmt = conn1.createStatement();
             ResultSet rs = stmt.executeQuery(Sql1);
@@ -40,10 +40,10 @@ public class BoutonRecherchePatient implements ActionListener {
                 String prenom = rs.getString("Prénom");
                 String ipp1 = rs.getString("IPP");
                 System.out.println(name + "\t" + prenom + "\t" + ipp1);
-                DefaultTableModel templatesTableModel = new DefaultTableModel(new String[][]{{name, prenom, ipp}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}}, new String[]{"Nom", "Prénom", "IPP"});
+                DefaultTableModel templatesTableModel = new DefaultTableModel(new String[][]{{name, prenom, ipp1}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}}, new String[]{"Nom", "Prénom", "IPP"});
                 cdpis.getjTable1().setGridColor(new java.awt.Color(153, 153, 153));
                 cdpis.getjTable1().setModel(templatesTableModel);
-                
+                cdpis.getjTable1().setFont(new java.awt.Font("Calibri", 0, 18));
 
             }
 
@@ -51,5 +51,36 @@ public class BoutonRecherchePatient implements ActionListener {
             e1.printStackTrace();
         }
 
-    }
-}
+        try {
+            String nomPrenom = cdpis.getjTextField1().getText();
+            String[] splitArray = null; //tableau de chaînes
+
+            // On découpe la chaîne à traiter et on récupère le résultat dans le tableau "splitArray"
+            splitArray = nomPrenom.split(" ");
+
+            for (int i = 0; i < splitArray.length; i++) {
+                // On affiche chaque élément du tableau
+                System.out.println("élement n° " + i + "=[" + splitArray[i] + "]");
+            }
+            String nom1 = splitArray[0];
+            String prenom1 = splitArray[1];
+            String Sql2 = "Select * FROM patient WHERE Nom ='" + nom1 + "'and Prénom ='" + prenom1 + "'";
+            java.sql.Connection conn1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd1" + "?serverTimezone=UTC", "armelle", "armelle");
+            Statement stmt = null;
+            stmt = conn1.createStatement();
+            ResultSet rs = stmt.executeQuery(Sql2);
+            while (rs.next()) {
+                String name = rs.getString("Nom");
+                String prenom = rs.getString("Prénom");
+                String ipp1 = rs.getString("IPP");
+                System.out.println(name + "\t" + prenom + "\t" + ipp1);
+                DefaultTableModel templatesTableModel = new DefaultTableModel(new String[][]{{name, prenom, ipp1}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}}, new String[]{"Nom", "Prénom", "IPP"});
+                cdpis.getjTable1().setGridColor(new java.awt.Color(153, 153, 153));
+                cdpis.getjTable1().setModel(templatesTableModel);
+                cdpis.getjTable1().setFont(new java.awt.Font("Calibri", 0, 18));
+
+            }
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+    }}
