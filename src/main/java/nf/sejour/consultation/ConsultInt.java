79dc1ab47babe2eeefa7dd161d76b2a5a5f229b2
Armelle Bernard <armellebernard@free.fr>
2019-02-27 13:08:08 +0100

    Ajout des modifications manquantes de reformatage

--- src/main/java/nf/sejour/consultation/ConsultInt.java
+++ src/main/java/nf/sejour/consultation/ConsultInt.java
@@ -1,11 +1,11 @@
-    package main.java.nf.sejour.consultation;
+package main.java.nf.sejour.consultation;
 
 import main.java.nf.personnel.Personnel;
 import main.java.nf.sejour.Prescription;
 
 import java.util.List;
 
-public class ConsultInt extends Consultation{
+public class ConsultInt extends Consultation {
 
     public ConsultInt(Personnel pH, List<Prescription> lp) {
         super(pH, lp);
--- src/main/java/nf/sejour/consultation/Consultation.java
+++ src/main/java/nf/sejour/consultation/Consultation.java
@@ -9,8 +9,8 @@ public abstract class Consultation {
     private Personnel pH;
     private List<Prescription> lp;
 
-    public Consultation(Personnel pH,List<Prescription> lp ) {
+    public Consultation(Personnel pH, List<Prescription> lp) {
         this.pH = pH;
-        this.lp=lp;
+        this.lp = lp;
     }
 }
